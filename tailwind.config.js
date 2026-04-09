ad27d02501792875b0606965273710872e12f34b
abellmanuell <mannydev02@gmail.com>
2026-04-09 02:04:30 +0100

    feat: Contact Us & Footer Completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter Tight", "sans-serif"],
+      },
+    },
+  },
+};
